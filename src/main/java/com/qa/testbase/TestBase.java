695bc5ee82dc5024cbf79e3ebaa34650e92bdd85
praveen <praveenreddy.narala@gmail.com>
2019-03-09 21:17:07 +0530

    Updated Test =Base.java --Added new arguments
    Added new test cases:
    REST_Assure_Get_Call.java

--- src/main/java/com/qa/testbase/TestBase.java
+++ src/main/java/com/qa/testbase/TestBase.java
@@ -1,13 +1,27 @@
 package com.qa.testbase;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class TestBase {
 
-	
+	//200 (OK)
+	//GET an entity corresponding to the requested resource is sent in the response;
+	public int RESPONSE_CODE_200 = 200;
+	public int RESPONSE_CODE_201 = 201;
+	public int RESPONSE_CODE_204 = 204;
+	//400 (Bad Request)
+	//400 is the generic client-side error status,
+	// used when no other 4xx error code is appropriate.
+	// Errors can be like malformed request syntax, invalid request message parameters, or deceptive request routing etc.
+	public int RESPONSE_CODE_400 = 400;
+	public int RESPONSE_CODE_401 = 401;
+	public int RESPONSE_CODE_500 = 500;
+
+	public String RESPONSE_STATUS_LINE = "HTTP/1.1 200 OK";
+	public String BAD_RESPONSE_STATUS_LINE = "HTTP/1.1 400 Bad Request";
+
 	public Properties prop;
 	
 	private static TestBase testBaseObj = null;
--- src/test/java/com/qa/test/REST_Assure_Get_Call.java
+++ src/test/java/com/qa/test/REST_Assure_Get_Call.java
@@ -3,6 +3,7 @@ package com.qa.test;
 import com.qa.restassureclient.RestAssureClientCall;
 import com.qa.testbase.TestBase;
 import io.restassured.response.ResponseBody;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,8 @@ public class REST_Assure_Get_Call {
     private String sURL;
     private String sCityName;
     private ResponseBody responseBody;
+    private int sStatusCode;
+    private String sStatusLine;
 
     @BeforeMethod
     public void setup(){
@@ -22,6 +25,36 @@ public class REST_Assure_Get_Call {
     public void getResponseBody(){
         responseBody = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, sCityName).getBody();
         System.out.println(responseBody.asString());
+    }
+
+    @Test
+    public void validate_get_response_status_code(){
+        sStatusCode = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, sCityName).getStatusCode();
+        System.out.println("Response status code-->"+sStatusCode);
+        Assert.assertEquals(sStatusCode, TestBase.getinstance().RESPONSE_CODE_200);
+    }
+
+    @Test
+    public void validate_get_incorrect_response_status_code(){
+        sStatusCode = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, "/Kamareddy").getStatusCode();
+        System.out.println("Response status code-->"+sStatusCode);
+        sStatusLine = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, "/Kamareddy").getStatusLine();
+        System.out.println("Response status line-->"+sStatusLine);
+        Assert.assertTrue(sStatusCode == TestBase.getinstance().RESPONSE_CODE_400, "Incorrect response status code-->"+sStatusCode);
+    }
+
+    @Test
+    public void validate_get_incorrect_response_status_line(){
+        sStatusLine = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, "/Kamareddy").getStatusLine();
+        System.out.println("Response status line-->"+sStatusLine);
+        Assert.assertTrue(sStatusLine.equalsIgnoreCase(TestBase.getinstance().BAD_RESPONSE_STATUS_LINE), "Incorrect response status line-->"+sStatusLine);
+    }
+
+    @Test
+    public void validate_get_response_status_line(){
+        sStatusLine = RestAssureClientCall.getInstance().getRESTAssureResponse(sURL, sCityName).getStatusLine();
+        System.out.println("Response status line-->"+sStatusLine);
+        Assert.assertEquals(sStatusLine, TestBase.getinstance().RESPONSE_STATUS_LINE, "Incorrect status line");
     }
 
 }
